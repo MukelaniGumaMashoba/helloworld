612b4283e8e129a6e6542ea82e0949f1f80e31b5
Mukelani Mashoba <mukelanilastborn@gmail.com>
2025-02-14 00:11:55 +0200

    helloworld

---
+++ hellowworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    // sdt : standard namespace, cout : console output
+    // always return 0 if the program is executed successfully and 1 if not
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World!" << '\n';
+
+    // variables int, float, double, char, string
+    return 0;
+}
