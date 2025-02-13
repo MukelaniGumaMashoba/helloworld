62c30c0030b525437ef050aaf7fc1184c371aa34
Mukelani Mashoba <mukelanilastborn@gmail.com>
2025-02-14 00:26:00 +0200

    no this is super long and tough yeeer

--- hellowworld.cpp
+++ hellowworld.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <vector>
 
-int main(){
-    // sdt : standard namespace, cout : console output
+int main()
+{
+    using std::cout;
+    using std::string;
+    // std : standard namespace, cout : console output
     // always return 0 if the program is executed successfully and 1 if not
-    std::cout << "Hello World!" << std::endl;
+    cout << "Hello World!" << std::endl;
     std::cout << "Hello World!" << '\n';
 
+    int a = 5;
+    cout << a;
     // variables int, float, double, char, string
     return 0;
 }
